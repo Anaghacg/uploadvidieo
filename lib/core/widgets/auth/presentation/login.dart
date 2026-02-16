b3d882fee39cb5b413750b6b92ca7471814ce890
Anagha C G <anaghaabi1994@gmail.com>
2026-02-16 21:53:38 +0530

    implemented login screen ui

--- lib/core/widgets/auth/presentation/login.dart
+++ lib/core/widgets/auth/presentation/login.dart
@@ -5,6 +5,44 @@ class LoginScreen extends StatelessWidget{
   const LoginScreen({super.key});
   @override
   Widget build (BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body:SafeArea(child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(children: [
+          SizedBox(height: 60,),
+          Text("Enter Your \nMobile Number",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+          SizedBox(height: 10,),
+          Text("We will send you OTP to verify your number.",style: TextStyle(color: Colors.grey),),
+                    SizedBox(height: 30,),
+       Container(padding: EdgeInsets.symmetric(horizontal: 12),
+                    decoration: BoxDecoration(color: Color(0xFF1A1D24),
+                    borderRadius: BorderRadius.circular(8)),
+                    child: Row(children: [
+                      Text(("+91"),
+                      ),SizedBox(width: 10,),
+                      Expanded(child: TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(border: InputBorder.none,
+                        hintText: "Enter Mobile Number"),
+                      ))
+                    ],),
+                    ),
+
+              Spacer(),
+              SizedBox(width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFE53935),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  ),
+                  onPressed: (){},
+                  child: Text("Continue"),
+                ),
+              ),SizedBox(height: 30,) 
+                    
+        ],),
+        ))
+    );
   }
 }
